c165c832a8906f86564cf8c8b7fe8959693995a1
GuidoGiustozzi <Guido.giustozzi@gmail.com>
2024-01-12 14:51:22 -0300

    [Realizo set get y _init_]

--- circle/circle.py
+++ circle/circle.py
@@ -2,3 +2,22 @@ import math
 
 class Circle():
     pass
+class Circle():
+    ### INICIALIZAMOS CON RADIUS
+    def __init__(self,radius) :
+        if radius >= 0 :
+            self.radius = radius
+        else:
+            raise ValueError('El radio debe ser mayor a 0')
+                               
+    def get_radius(self):
+        return self.radius
+
+    def set_radius(self,radius):
+        if radius >= 0 :
+            self.radius = radius
+        else:
+            raise ValueError('El radio debe ser mayor a 0')
+                
+        return self.radius 
+    
