64ea49bde2495c192327ea0f92d75e94c37ea2e4
GuidoGiustozzi <Guido.giustozzi@gmail.com>
2024-01-12 14:30:07 -0300

    [Realizo ejercicio simple]

--- simple/simple.py
+++ simple/simple.py
@@ -1,9 +1,23 @@
-import random
-
+from random import *
 
 def simple_list():
-    pass
+    # CREO LA LISTA Y RECORRO 10 veces
+    list = []
+    for x in range(10):
+        dict = {}
+        dict['id'] = x
+        dict['age'] = randint(0, 100)  # NUMERO ALEATORIO DE 0 a 100
+        list.append(dict)  # AGREGO A LA LISTA
+    return list
 
+def sort_list(dicts):
+    return sorted(dicts, key=ordenar_edad_ascendente)  # UTILIZO SORTED
 
-def sort_list():
-    pass
+def ordenar_edad_ascendente(dicts):
+    return dicts['age']
+
+Lista = simple_list()
+print(Lista)
+ordenado_edad = sort_list(Lista)
+print(ordenado_edad)
+
